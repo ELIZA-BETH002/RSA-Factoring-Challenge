7ee23ec626451d22bd3810487729b46fa84cde86
ELIZA-BETH002 <106850645+ELIZA-BETH002@users.noreply.github.com>
2023-05-29 12:12:49 +0100

    Factors

--- factors.py
+++ factors.py
@@ -1,39 +1,32 @@
-import sys
+#!/usr/bin/python3
 
-def factorize(num):
-    factors = []
-    for i in range(2, int(num**0.5) + 1):
-        while num % i == 0:
-            factors.append(i)
-            num //= i
-    if num > 1:
-        factors.append(num)
-    return factors
+from sys import argv
 
-def main():
-    if len(sys.argv) != 2:
-        print("Usage: factors <file>")
-        return
-    
-    input_file = sys.argv[1]
-    
-    try:
-        with open(input_file, r) as file:
-            numbers = [int(line.strip()) for line in file]
-    except FileNotFoundError:
-        print(f"File {input_file} not found.")
-        return
-    except ValueError:
-        print("Invalid input in the file. Only natural numbers greater than 1 are allowed.")
-        return
-    
-    for num in numbers:
-        factors = factorize(num)
-        if len(factors) >= 2:
-            p = factors[0]
-            q = factors[1]
-            print(f"{num}={p}*{q}")
-    
-if __name__ == "__main__":
-    main()
 
+def factorize(value):
+    """"print a simple descomposition of an integer > 1"""
+    i = 2
+
+    if value < 2:
+        return
+    print()
+    print(value, "<- value-bef")
+    while value % i:
+        i += 1
+    print("{:.0f}={:.0f}*{:.0f}".format(value, value / i, i))
+    print(value, "<- value-aft")
+    print()
+
+if len(argv) != 2:
+    exit()
+
+try:
+    with open(argv[1]) as file:
+        line = file.readline()
+
+        while line != "":
+            value = int(line.split('\n')[0])
+            factorize(value)
+            line = file.readline()
+except:
+    pass
